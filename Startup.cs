0ca7692944e6c6e869ce7135eaecff33ac524654
Christian LeVesque <christian@christianlevesque.io>
2020-12-10 21:55:07 -0600

    Added basic middleware to add `X-Application-Purpose` response header

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 
 namespace timestamp
@@ -12,6 +13,17 @@ namespace timestamp
 			{
 				app.UseDeveloperExceptionPage();
 			}
+
+			app.Use(async (context, next) =>
+			{
+				context.Response.Headers.Add("X-Application-Purpose", "Timestamp Microservice");
+				await next.Invoke();
+			});
+
+			app.Run(async context =>
+			{
+				await context.Response.WriteAsync("You got an extra header!");
+			});
 		}
 	}
 }
