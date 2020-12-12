061f2b15fb3c2dd16141a263c6e1954806384695
Christian LeVesque <christian@christianlevesque.io>
2020-12-11 22:47:02 -0600

    Added CORS support to the app

--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,34 @@
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace timestamp
 {
 	public class Startup
 	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddCors(options =>
+			{
+				options.AddPolicy("AllowFcc", policy =>
+				{
+					policy.WithOrigins("https://www.freecodecamp.org");
+					policy.WithMethods("GET");
+				});
+			});
+		}
+		
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
+
+			app.UseCors("AllowFcc");
 		}
 	}
 }
